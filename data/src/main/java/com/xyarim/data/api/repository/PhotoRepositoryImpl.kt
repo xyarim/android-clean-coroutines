907e26484c6a5dcbb9398d4e9a6444be2bb6b73a
Arslan Tazhibaev <arslan22011@gmail.com>
2019-11-19 20:26:57 +0500

    changed signature of PhotoRepositiry

--- data/src/main/java/com/xyarim/data/api/repository/PhotoRepositoryImpl.kt
+++ data/src/main/java/com/xyarim/data/api/repository/PhotoRepositoryImpl.kt
@@ -1,2 +1,20 @@
 package com.xyarim.data.api.repository
 
+import com.xyarim.data.api.service.PhotoService
+import com.xyarim.domain.models.Photo
+import com.xyarim.domain.repository.PhotoRepository
+import com.xyarim.domain.usecase.base.Error
+import com.xyarim.domain.usecase.base.Result
+
+class PhotoRepositoryImpl(val photoService: PhotoService) : PhotoRepository {
+    override suspend fun getPhotos(page: Int): Result<List<Photo>, Error> {
+        val response = photoService.getPhotos(page = page).await()
+        if (response.isSuccessful && response.body() != null) {
+            return Result.Success(response.body()!!.map {
+                return@map Photo(it.id, url = it.imageUrls.raw)
+            })
+        } else {
+            return Result.Failure();
+        }
+    }
+}
--- domain/src/main/java/com/xyarim/domain/repository/PhotoRepository.kt
+++ domain/src/main/java/com/xyarim/domain/repository/PhotoRepository.kt
@@ -1,6 +1,8 @@
 package com.xyarim.domain.repository
 
 import com.xyarim.domain.models.Photo
+import com.xyarim.domain.usecase.base.Error
+import com.xyarim.domain.usecase.base.Result
 
 /**
  * Interface that represents a Repository for getting [Photo] related data.
@@ -9,6 +11,6 @@ interface PhotoRepository {
     /**
      * Get a List of [Photo].
      */
-    suspend fun getPhotos(offset: Int): List<Photo>
+    suspend fun getPhotos(page: Int): Result<List<Photo>, Error>
 
 }
