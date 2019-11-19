08bbd2d38a7705060ec6cbad41673a78466f7075
Arslan Tazhibaev <arslan22011@gmail.com>
2019-11-19 20:29:05 +0500

    created GetPhotoUseCase

--- domain/src/main/java/com/xyarim/domain/usecase/GetPhotosUseCase.kt
+++ domain/src/main/java/com/xyarim/domain/usecase/GetPhotosUseCase.kt
@@ -1,2 +1,18 @@
 package com.xyarim.domain.usecase
 
+import com.xyarim.domain.repository.PhotoRepository
+import com.xyarim.domain.usecase.base.BaseUseCase
+import com.xyarim.domain.usecase.base.Result
+
+class GetPhotosUseCase(private val photoRepository: PhotoRepository) : BaseUseCase<Int>() {
+    override suspend fun run(params: Int) {
+        // Started loading
+        resultChannel.send(Result.State.Loading())
+
+        // Get person from persistence and send it, synchronous
+        resultChannel.send(photoRepository.getPhotos(params))
+
+
+        resultChannel.send(Result.State.Loaded())
+    }
+}
